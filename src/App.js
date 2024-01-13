29995aa8270bd0cea752fbe7a6319d88d844189e
Shahd-Bilal0 <hamzashahd001@gmail.com>
2024-01-13 16:05:04 +0200

    scroll-to-top button chevron-up

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import Error404 from './Pages/Error404';
 import Footer from './Components/Footer/Footer';
 import Register from './Pages/Register';
 import ScrollToTop from "react-scroll-to-top";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function App() {
   return (
       <BrowserRouter basename="/technolab">
-      <ScrollToTop smooth width='16' height='16' viewBox="0 0 512 512" svgPath="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"/>
-        <Header />
+      <ScrollToTop smooth
+      component={<FontAwesomeIcon icon="fa-solid fa-chevron-up" />}/>
+      <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
